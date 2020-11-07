605914d2c4487928e4e7201a81b3bf43120c7e87
Katarzyna <kataszl768@student.polsl.pl>
2020-11-07 17:31:58 +0100

    correction

--- spanning_tree.cpp
+++ spanning_tree.cpp
@@ -4,31 +4,37 @@ using namespace std;
 
 int main ()
 {
-        int a,b,c,d,e,f,g,h,i,j,k,l;
+    int a,b,c,d;
 
     cout<<"the graph looks as follows: "<<endl;
     cout<<endl;
 
-    cout<<"   2-----5"<<endl;
-    cout<<"  /  \\ /  \\ "<<endl;
-    cout<<"  1---4---7"<<endl;
-    cout<<"  \\  / \\  /"<<endl;
-    cout<<"   3-----6"<<endl;
+    cout<<"           a"<<endl;
+    cout<<"      1---------2"<<endl;
+    cout<<"      |         |"<<endl;
+    cout<<"    d |         | b"<<endl;
+    cout<<"      |         |"<<endl;
+    cout<<"      3---------4"<<endl;
+    cout<<"           c"<<endl;
 
     cout<<endl;
-    cout<<"please introduce the weights on each edge: "<<endl;
+
+    cout<<"        (1,2,a)"<<endl;
+    cout<<"        (2,4,b)"<<endl;
+    cout<<"        (4,3,c)"<<endl;
+    cout<<"        (3,1,d)"<<endl;
+
+    cout<<endl;
+    cout<<endl;
+
+    cout<<"please introduce the weights of the edges: "<<endl;
     cout<<"1,2,"; cin>>a;
-    cout<<"1,4,"; cin>>b;
-    cout<<"1,3,"; cin>>c;
-    cout<<"2,5,"; cin>>f;
-    cout<<"2,4,"; cin>>d;
-    cout<<"3,4,"; cin>>e;
-    cout<<"3,6,"; cin>>g;
-    cout<<"4,5,"; cin>>h;
-    cout<<"4,7,"; cin>>j;
-    cout<<"4,6,"; cin>>i;
-    cout<<"5,7,"; cin>>k;
-    cout<<"6,7,"; cin>>l;
+    cout<<"2,4,"; cin>>b;
+    cout<<"4,3,"; cin>>c;
+    cout<<"3,1,"; cin>>d;
+
+
 
     return 0;
 }
+
